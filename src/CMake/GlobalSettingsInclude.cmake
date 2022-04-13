3b9f37fd0c8e0375b81e201b2c7ba7ea965e5567
Greg Utas <greg@pentennea.com>
2022-04-13 16:14:28 -0400

    Changes for CMake builds.

--- src/CMake/GlobalSettingsInclude.cmake
+++ src/CMake/GlobalSettingsInclude.cmake
@@ -1,19 +1,34 @@
 # Options common to all RSC builds
 #
-if(MSVC)
-    message("Reading GlobalSettingsInclude.cmake")
+if((CMAKE_CXX_COMPILER_ID STREQUAL "MSVC") OR
+  (CMAKE_CXX_COMPILER_ID STREQUAL "Clang"))
+    message("Reading GlobalSettingsInclude.cmake shared by MSVC and CLang")
 
+    # Clang is currently used only to target Windows
     # Enable Windows targets (*.win.cpp files)
     add_compile_definitions(OS_WIN)
 
     # Use async exception handling to catch structured exceptions
     add_compile_options(/EHa)
 
-    # Include run-time checks
-    add_compile_options(/RTC1)
-
     # Disable optimizations to improve debugging
     add_compile_options(/Od)
+
+    # Keep frame pointers
+    add_compile_options(/Oy-)
+
+    # Include security checks
+    add_compile_options(/GS)
+
+    # Enable all compiler warnings
+    add_compile_options(/Wall)
+endif()
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    message("Reading GlobalSettingsInclude.cmake for MSVC")
+
+    # Include run-time checks
+    add_compile_options(/RTC1)
 
     # Provide full debugging information
     add_compile_options(/Z7)
@@ -21,17 +36,8 @@ if(MSVC)
     # Disable Just My Code debugging
     add_compile_options(/JMC-)
 
-    # Keep frame pointers
-    add_compile_options(/Oy-)
-
     # Use standard C++
     add_compile_options(/permissive-)
-
-    # Include security checks
-    add_compile_options(/GS)
-
-    # Enable all compiler warnings
-    add_compile_options(/Wall)
 
     # Treat compiler security warnings as errors
     add_compile_options(/sdl)
@@ -53,4 +59,49 @@ if(MSVC)
 
     # Support incremental linking for patching
     add_link_options(/INCREMENTAL)
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    message("Reading GlobalSettingsInclude.cmake for Clang")
+
+    # Provide full debugging information
+    add_compile_options(/Zi)
+
+    # Temporarily disable specific compiler warnings
+    add_compile_options(-Wno-defaulted-function-deleted)
+    add_compile_options(-Wno-reserved-identifier)
+    add_compile_options(-Wno-signed-enum-bitfield)
+    add_compile_options(-Wno-tautological-type-limit-compare)
+
+    # Disable specific compiler warnings
+    add_compile_options(-Wno-c++98-compat)
+    add_compile_options(-Wno-c++98-compat-pedantic)
+    add_compile_options(-Wno-cast-align)
+    add_compile_options(-Wno-cast-qual)
+    add_compile_options(-Wno-char-subscripts)
+    add_compile_options(-Wno-covered-switch-default)
+    add_compile_options(-Wno-deprecated-copy-with-dtor)
+    add_compile_options(-Wno-documentation)
+    add_compile_options(-Wno-exit-time-destructors)
+    add_compile_options(-Wno-global-constructors)
+    add_compile_options(-Wno-header-hygiene)
+    add_compile_options(-Wno-implicit-fallthrough)
+    add_compile_options(-Wno-implicit-int-conversion)
+    add_compile_options(-Wno-implicit-int-float-conversion)
+    add_compile_options(-Wno-inconsistent-missing-destructor-override)
+    add_compile_options(-Wno-missing-noreturn)
+    add_compile_options(-Wno-old-style-cast)
+    add_compile_options(-Wno-shadow)
+    add_compile_options(-Wno-shadow-field-in-constructor)
+    add_compile_options(-Wno-shorten-64-to-32)
+    add_compile_options(-Wno-sign-compare)
+    add_compile_options(-Wno-sign-conversion)
+    add_compile_options(-Wno-suggest-destructor-override)
+    add_compile_options(-Wno-switch)
+    add_compile_options(-Wno-switch-enum)
+    add_compile_options(-Wno-tautological-undefined-compare)
+    add_compile_options(-Wno-trigraphs)
+    add_compile_options(-Wno-unused-parameter)
+    add_compile_options(-Wno-unreachable-code-break)
+    add_compile_options(-Wno-unreachable-code-return)
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    message("Reading GlobalSettingsInclude.cmake for GCC")
 endif()
